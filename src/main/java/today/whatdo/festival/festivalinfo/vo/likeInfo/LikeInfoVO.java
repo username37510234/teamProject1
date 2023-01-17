c3387c6a52e92eb2c2c71381b202b277bae6ada0
user <user@i7B-33>
2023-01-17 15:34:11 +0900

    좋아요 VO+mapper(수정중)

---
+++ src/main/java/today/whatdo/festival/festivalinfo/vo/likeInfo/LikeInfoVO.java
@@ -0,0 +1,14 @@
+package today.whatdo.festival.festivalinfo.vo.likeInfo;
+
+import lombok.Data;
+
+@Data
+public class LikeInfoVO {
+   
+   private int liNum;
+   private boolean liActive;
+   private int uiNum;
+   private int fiNum;
+   
+
+}
