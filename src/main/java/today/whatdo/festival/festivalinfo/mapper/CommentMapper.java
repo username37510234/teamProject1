e0547c9dd0ed4342fe7726f141b1f23fe220cfae
user <user@i7B-25>
2023-01-27 15:41:41 +0900

    comment 수정

--- src/main/java/today/whatdo/festival/festivalinfo/mapper/CommentMapper.java
+++ src/main/java/today/whatdo/festival/festivalinfo/mapper/CommentMapper.java
@@ -6,8 +6,8 @@ import today.whatdo.festival.festivalinfo.vo.commentInfo.CommentVO;
 
 public interface CommentMapper {
 	void CommentRegist(CommentVO vo); //댓글 등록
-	List<CommentVO> getList(int com_bno); //목록 요청
-	int getTotal(int com_bno); //댓글 개수
-	int commentDelete(CommentVO vo); //댓글 삭제
+	List<CommentVO> getList(int fiNum); //목록 요청
+	int getTotal(int fiNum); //댓글 개수
+	int commentDelete(int ciNum); //댓글 삭제
 	
 }
